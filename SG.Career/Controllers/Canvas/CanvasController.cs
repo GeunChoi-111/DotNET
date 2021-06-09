a3ca76a1b2b4cc1b76d38c0582a189573bc10e26
Sanggeun Choi <sanggeun.choi@marineworks.co.kr>
2021-06-09 19:03:27 +0900

    최초 등록

---
+++ SG.Career/Controllers/Canvas/CanvasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SG.Career.Controllers.Canvas
+{
+    public class CanvasController : Controller
+    {
+        // GET: Canvas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BounceString()
+        {
+            return View();
+        }
+    }
+}
---
+++ SG.Career/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SG.Career.MvcApplication" Language="C#" %>
