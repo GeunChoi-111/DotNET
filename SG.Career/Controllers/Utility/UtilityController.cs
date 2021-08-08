199fca91f37e8534ab2315b42d2b94477443d7b1
Sanggeun Choi <sanggeun.choi@marineworks.co.kr>
2021-08-08 17:52:53 +0900

    Email 발송

---
+++ SG.Career/Controllers/Utility/UtilityController.cs
@@ -0,0 +1,64 @@
+﻿using SG.Career.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SG.Career.Controllers.Utility
+{
+    public class UtilityController : Controller
+    {
+        // GET: Utility
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SendEmail()
+        {
+            int rs = 0;
+
+            string _host = "smtp.office365.com";
+            int _port = 587; //기본 smtp port : 25 , office365 587
+            string _userId = "";
+            string _userPassword = "";
+            string fromEmail = "sanggeun.choi@marineworks.co.kr";
+            string fromName = "상근";
+            string to = "suhyang923@naver.com";
+
+            string cc = "";
+            string subject = "수향이 안녕?";
+            string body = "코딩으로 메일보내긔!";
+            bool IsHtml = false;
+
+            MailMessage message = SetMailMessage(fromEmail, fromName, to, cc, subject, body, IsHtml);
+            Email email = new Email(_host, _port, _userId, _userPassword, message);
+            email.Send();
+
+            return Json(new { RS = rs });
+        }
+
+
+        public MailMessage SetMailMessage(string fromEmail, string fromName, string to, string cc, string subject, string body, bool IsHtml)
+        {
+            MailMessage message = new MailMessage();
+
+            MailAddress address = new MailAddress(fromEmail, fromName);
+
+            message.From = address;
+            message.To.Add(to);
+            if (!string.IsNullOrEmpty(cc))
+            {
+                message.CC.Add(cc);
+            }
+            message.Subject = subject;
+            message.Body = body;
+            message.IsBodyHtml = IsHtml;
+
+            return message;
+        }
+    }
+}
---
+++ SG.Career/Utility/Email.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG.Career.Utility
+{
+    interface IEmail
+    {
+        void Send();
+    }
+
+    public class Email
+    {
+        public string _host;
+        public int _port;
+        public string _userId;
+        public string _userPassword;
+        public MailMessage _mailMassage;
+
+        public Email() 
+        {
+            this._host = ConfigurationManager.AppSettings["SmtpHost"];
+            this._port = Convert.ToInt32(ConfigurationManager.AppSettings["SmtpPort"]);
+            this._userId = string.IsNullOrEmpty(ConfigurationManager.AppSettings["SmtpUserId"]) ? "" : ConfigurationManager.AppSettings["SmtpUserId"];
+            this._userPassword = string.IsNullOrEmpty(ConfigurationManager.AppSettings["SmtpUserPassword"]) ? "" : ConfigurationManager.AppSettings["SmtpUserPassword"];
+        }
+
+
+        public Email(string _host, int _port, string _userId, string _userPassword, MailMessage _mailMassage)
+        {
+            this._host = _host;
+            this._port = _port;
+            this._userId = _userId;
+            this._userPassword = _userPassword;
+            this._mailMassage = _mailMassage;
+        }
+
+        public void Send()
+        {
+            try
+            {
+                using (System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(_host, _port))
+                {
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.EnableSsl = true;
+                    smtp.Credentials = new System.Net.NetworkCredential(_userId, _userPassword);
+                    smtp.Send(_mailMassage);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString(), ex.InnerException);
+            }
+            finally
+            {
+                _mailMassage.Dispose();
+            }
+        }
+    }
+}
